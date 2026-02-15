e8ee4c70ae2f86a4f1ebd5fb9c0c1f783dd662b4
Northskill <daniil2007tarasuk@gmail.com>
2026-02-16 00:20:56 +0300

    Add files via upload

---
+++ data/products/chevrolet_equinox.js
@@ -0,0 +1,1 @@
+window['PRODUCTS_CHEVROLET_EQUINOX'] = [];
---
+++ data/products/chevrolet_malibu.js
@@ -0,0 +1,91 @@
+window['PRODUCTS_CHEVROLET_MALIBU'] = [
+    {
+        "type": "Все",
+        "brand": "фыв",
+        "art": "111111",
+        "price": 111111,
+        "images": [
+            "111111_0.jpg"
+        ],
+        "stock": true
+    },
+    {
+        "type": "Все",
+        "brand": "вфы",
+        "art": "вфы",
+        "price": 13222,
+        "images": [
+            "____0.jpg"
+        ],
+        "stock": true
+    },
+    {
+        "type": "Все",
+        "brand": "Bosh",
+        "art": "123",
+        "price": 123,
+        "desc": "asd",
+        "images": [
+            "123_0.jpg"
+        ],
+        "stock": true
+    },
+    {
+        "type": "Все",
+        "brand": "asd",
+        "art": "12",
+        "price": 1222,
+        "desc": "",
+        "images": [
+            "12_0.jpg"
+        ],
+        "stock": true
+    },
+    {
+        "type": "Все",
+        "brand": "Ищыр",
+        "art": "12",
+        "price": 122,
+        "desc": "",
+        "images": [
+            "12_0.jpg"
+        ],
+        "stock": true
+    },
+    {
+        "type": "Все",
+        "brand": "bosh",
+        "art": "1111",
+        "price": 1111,
+        "desc": "",
+        "images": [
+            "1111_0.jpg",
+            "1111_1.jpg",
+            "1111_2.jpg",
+            "1111_3.jpg",
+            "1111_4.jpg",
+            "1111_5.jpg",
+            "1111_6.jpg",
+            "1111_7.jpg",
+            "1111_8.jpg",
+            "1111_9.jpg",
+            "1111_10.jpg",
+            "1111_11.jpg",
+            "1111_12.jpg",
+            "1111_13.png",
+            "1111_14.jpg"
+        ],
+        "stock": true
+    },
+    {
+        "type": "Все",
+        "brand": "ыамвпы",
+        "art": "111",
+        "price": 111,
+        "desc": "",
+        "images": [
+            "default.jpg"
+        ],
+        "stock": true
+    }
+];
